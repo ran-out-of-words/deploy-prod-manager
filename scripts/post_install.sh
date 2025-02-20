12ac2663fbc147ff8b146211cc5be361de0b1011
ran-out-of-words <er12345.ky@gmail.com>
2025-02-20 06:47:13 +0000

    Test AppSpec 11

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -4,3 +4,4 @@ set -e
 mkdir -p storage/framework/{cache,data,sessions,testing,views}
 mkdir -p storage/bootstrap/cache
 chown www-data:www-data -R storage/framework/{cache,data,sessions,testing,views}
+chown www-data:www-data -R storage/bootstrap/cache
