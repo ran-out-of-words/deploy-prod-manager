bc2851fe5fa3592d3f9b0c8ae2cfe30379143196
ran-out-of-words <er12345.ky@gmail.com>
2025-02-20 03:42:53 +0000

    Test AppSpec 1

--- scripts/install_system_dependencies.sh
+++ scripts/install_system_dependencies.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 sudo apt update -y
-sudo apt install composer -y
+sudo apt upgrade -y
+sudo add-apt-repository ppa:ondrej/php
+sudo apt install git php8.3 php8.3-cli php8.3-fpm php8.3-redis php8.3-gd php8.3-readline php8.3-mysql php8.3-xml php8.3-mbstring php8.3-curl php8.3-intl php8.3-zip composer -y
