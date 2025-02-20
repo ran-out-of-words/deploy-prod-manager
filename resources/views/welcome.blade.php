53a863174751225e28c7d46e769524e611d4e5c9
ran-out-of-words <er12345.ky@gmail.com>
2025-02-20 06:52:33 +0000

    Test Deploy 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1107,7 +1107,7 @@
                         </div>
                     </div>
                 </main>
-
+                <h1>Hello World</h1>
                 <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                     Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                 </footer>
