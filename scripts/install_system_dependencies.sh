ae022b86e09c252b7419545819a7276f933cbb7b
ran-out-of-words <er12345.ky@gmail.com>
2025-02-20 03:15:34 +0000

    Test 9

--- scripts/install_system_dependencies.sh
+++ scripts/install_system_dependencies.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 sudo apt update -y
-sudo apt upgrade -y
-sudo apt install git php8.1 php8.1-cli php8.1-fpm php8.1-redis php8.1-gd php8.1-readline php8.1-mysql php8.1-xml php8.1-mbstring php8.1-curl php8.1-intl php8.1-zip composer -y
+sudo apt install composer -y
