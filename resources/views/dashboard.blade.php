6d2f42551c3fad14d05145abd11d985a777e8be1
ran-out-of-words <er12345.ky@gmail.com>
2025-02-20 07:20:42 +0000

    Test Deploy 3

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
         <div class="container">
             <a class="navbar-brand" href="#">
                 <i class="fas fa-box-open me-2"></i>
-                Product Management
+                Tentang Produk
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
